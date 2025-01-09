c01eb5a47de3e850b47fde4cce61e12205664359
Kevin Serrano <kevinserrano010@gmail.com>
2025-01-09 11:48:52 -0300

    Cambio de nombre de la aplicacion

--- api/urls.py
+++ api/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('servicesApp.urls')),
+    path('api/', include('services.urls')),
 ]
--- services/apps.py
+++ services/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ServicesappConfig(AppConfig):
+class ServicesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'servicesApp'
+    name = 'services'
