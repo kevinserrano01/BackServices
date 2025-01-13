be371cd92c5c186041f41600151b43e9cae69c85
Kevin Serrano <kevinserrano010@gmail.com>
2025-01-13 14:14:35 -0300

    Creacion de admin para interactions y users.

--- interactions/admin.py
+++ interactions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Posts, Requests, StatusServices
 
 # Register your models here.
+admin.site.register(Posts)
+admin.site.register(Requests)
+admin.site.register(StatusServices)
