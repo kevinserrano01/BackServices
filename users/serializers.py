23cd04d221c4d0ea060e1439de387fe27b0d6b8e
Cristian Villa <vcristian528@gmail.com>
2025-01-14 13:43:55 -0300

    + implementacion jwt + implementacion de algunos metodos

--- users/serializers.py
+++ users/serializers.py
@@ -7,12 +7,16 @@ class UsersSerializer(serializers.ModelSerializer):
         model = Users
         # Campos visibles en la API
         fields = ['id', 'username', 'email', 'first_name', 'last_name',
-                  'telephone', 'is_supplier', 'is_finder', 'created_at', 'updated_at']
+                  'telephone', 'is_supplier', 'is_finder', 'created_at', 'updated_at','password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
         """Crea un usuario con la contraseña encriptada"""
+        password = validated_data.pop('password')
         user = Users.objects.create_user(**validated_data)
+        user.set_password(password)
+        user.is_active = True
+        user.save()
         return user
 
 
@@ -20,3 +24,6 @@ class RatingsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ratings
         fields = '__all__'
+    def create(self, validated_data):
+        rating = Ratings.objects.create(**validated_data)
+        return rating
--- users/views.py
+++ users/views.py
@@ -1,5 +1,7 @@
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework import status
 from .models import Users, Ratings
@@ -7,12 +9,18 @@ from .serializers import UsersSerializer, RatingsSerializer
 
 # Create your views here.
 
+class UserRegisterView(generics.CreateAPIView):
+     queryset = Users.objects.all() 
+     serializer_class = UsersSerializer 
+     permission_classes = [AllowAny] 
 
 class UsersList(generics.ListAPIView):
     """Vista para listar usuarios"""
     queryset = Users.objects.all()
     serializer_class = UsersSerializer
 
+class CustomTokenObtainPairView(TokenObtainPairView):
+    permission_classes = [AllowAny]
 
 class UsersDetail(generics.RetrieveUpdateDestroyAPIView):
     """Vista para ver, editar y eliminar usuarios"""
@@ -25,6 +33,8 @@ class RatingsList(generics.ListCreateAPIView):
     """Vista para listar y crear ratings"""
     queryset = Ratings.objects.all()
     serializer_class = RatingsSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
 
 
 class RatingsDetail(generics.RetrieveUpdateDestroyAPIView):
@@ -32,3 +42,4 @@ class RatingsDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Ratings.objects.all()
     serializer_class = RatingsSerializer
     lookup_field = 'id'  # Permite buscar por el campo 'id'
+    permission_classes = [IsAuthenticated]
