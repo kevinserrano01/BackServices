47b80eb388416d4e280474b27c2b78cb96d74453
Cristian Villa <vcristian528@gmail.com>
2025-01-29 15:01:18 -0300

    allowed hosts incorrectos

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['servify-api.netlify.app']
+ALLOWED_HOSTS = ['*']
 CORS_ALLOW_ALL_ORIGINS = True
 
 
