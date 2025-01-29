80375e4814fcc8b7f927309d9032d173e02671e5
Cristian Villa <vcristian528@gmail.com>
2025-01-29 14:43:22 -0300

    + fix allowed host del front

--- api/settings.py
+++ api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://servify-api.netlify.app',]
 CORS_ALLOW_ALL_ORIGINS = True
 
 
